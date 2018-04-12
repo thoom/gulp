d8f5c7a75721ae0fb51b9fef5723c30ab8a920a0
Zach Peacock <1316813+thoom@users.noreply.github.com>
2018-04-12 00:53:49 -0600

    Release take 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
+docker run --rm -it -v $PWD:/go/src/gulp -w /go/src/gulp golang:alpine sh -c "apk add --update --no-cache git && go get -d ./... && go build -o gulp-alpine"
 docker build -t gulp -f Dockerfile-alpine .
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
