8e1b7cb74bb619ae5801e7c6ba0c3358f72e6476
Zach Peacock <1316813+thoom@users.noreply.github.com>
2018-07-05 14:25:34 -0600

    Simplified Docker deploy process

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
-docker run --rm -it -v $PWD:/go/src/github.com/thoom/gulp -w /go/src/github.com/thoom/gulp -e TRAVIS_BRANCH golang:alpine sh -c "apk add --update --no-cache git && go get -d ./... && go build -ldflags \"-X github.com/thoom/gulp/client.buildVersion=$TRAVIS_BRANCH\" -o gulp-alpine"
-docker build -t gulp -f Dockerfile-alpine --no-cache .
+docker build -t gulp -f Dockerfile-alpine --no-cache --build-arg BUILD_VERSION=$TRAVIS_BRANCH .
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker tag gulp thoom/gulp:latest
